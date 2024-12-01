3820f99739b96ef4dd3ffc739b15165686f91c06
Adam Tajti <adam.tajti@gmail.com>
2024-12-01 20:58:03 +0100

    nvim: inputs for snippet prefix and description

--- files/.config/nvim/lua/plugins/lazy/deus.nvim/lua/deus/snippets.lua
+++ files/.config/nvim/lua/plugins/lazy/deus.nvim/lua/deus/snippets.lua
@@ -2,9 +2,9 @@ local utils = require("deus.utils")
 
 local M = {}
 
-function M.selection_to_vscode_snippet(id, description, string_array)
-  local snippet = '  "' .. id .. '": {\n'
-  snippet = snippet .. '    "prefix": ["' .. id .. '"],\n'
+function M.selection_to_vscode_snippet(prefix, description, string_array)
+  local snippet = '  "' .. description .. '": {\n'
+  snippet = snippet .. '    "prefix": ["' .. prefix .. '"],\n'
   snippet = snippet .. '    "body": [\n'
 
   for _, entry in ipairs(string_array) do
@@ -35,6 +35,46 @@ function M.selection_to_lua_snippet(id, description, string_array)
   return snippet
 end
 
+function M._handle_description_input(description)
+  print("entered description: " .. description)
+  if description == nil or description == "" then
+    print("description nil or empty, esc")
+    return
+  end
+
+  local snippet = M.selection_to_vscode_snippet(
+    M.context_prefix,
+    description,
+    M.context_content
+  )
+
+  utils.copy_to_clipboard(snippet)
+end
+
+function M._handle_prefix_input(prefix)
+  print("entered prefix: " .. prefix)
+  if prefix == nil or prefix == "" then
+    print("prefix nil or empty, esc")
+    return
+  end
+
+  M.context_prefix = prefix
+
+  vim.ui.input(
+    { prompt = "snippet description: " },
+    M._handle_description_input
+  )
+end
+
+function M.copy_selection_to_clipboard()
+  -- Get content from visual selection
+  local lines = utils.get_visual_selection()
+  local content = utils.ensure_string_array(lines)
+
+  M.context_content = content
+  vim.ui.input({ prompt = "snippet prefix: " }, M._handle_prefix_input)
+end
+
 function M.setup()
   vim.api.nvim_set_keymap("n", "<Leader>Ds", "", {
     noremap = true,
@@ -46,19 +86,7 @@ function M.setup()
     desc = "Copy to Clipboard as a VSCode snippet",
     noremap = true,
     silent = true,
-    callback = function()
-      -- Get content from visual selection
-      local lines = utils.get_visual_selection()
-      local content = utils.ensure_string_array(lines)
-
-      local snippet = M.selection_to_vscode_snippet(
-        "first-snippet-test-without-id",
-        "First Snippet Test Without ID",
-        content
-      )
-
-      utils.copy_to_clipboard(snippet)
-    end,
+    callback = M.copy_selection_to_clipboard,
   })
 
   vim.api.nvim_set_keymap("n", "<Leader>So", "", {
