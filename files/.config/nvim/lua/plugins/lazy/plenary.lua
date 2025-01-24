ad156cf44737eb77dfb177c53f3283de3b4722e0
Adam Tajti <adam.tajti@gmail.com>
2025-01-24 11:02:31 +0100

    plenary +keymap to reload obsidian source for blink

--- files/.config/nvim/lua/plugins/lazy/plenary.lua
+++ files/.config/nvim/lua/plugins/lazy/plenary.lua
@@ -3,12 +3,30 @@ return {
   "nvim-lua/plenary.nvim",
   init = function()
     -- The keymap was much more simpler than this, but I wanted to use <Leader>p for :pwd
-    vim.api.nvim_set_keymap("n", "<leader>bPT", "", {
-      desc = "Test Current Buffer",
-      noremap = true,
-      callback = function()
+    vim.keymap.set(
+      "n",
+      "<leader>bPT",
+      function()
         require("plenary.test_harness").test_directory(vim.fn.expand("%:p"))
       end,
-    })
+      {
+        desc = "Test Current Buffer",
+        noremap = true,
+      }
+    )
+
+    vim.keymap.set(
+      "n",
+      "<leader>bPo",
+      function()
+        require("plenary.reload").reload_module(
+          "obsidian.completion.sources.blink.cmp_blink"
+        )
+      end,
+      {
+        desc = "Reload the Obsidian source for Blink",
+        noremap = true,
+      }
+    )
   end,
 }
