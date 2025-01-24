3a23a277870296ee9b121337033ce0ee2167a695
Adam Tajti <adam.tajti@gmail.com>
2025-01-24 11:13:41 +0100

    nvim(sort): a handy plugin to sort selected lines

---
+++ files/.config/nvim/lua/plugins/lazy/sort.lua
@@ -0,0 +1,13 @@
+return {
+  "sQVe/sort.nvim",
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>lrs",
+      "<esc><cmd>Sort<CR>",
+      desc = "Sort",
+      mode = "v",
+      silent = true,
+    },
+  },
+}
