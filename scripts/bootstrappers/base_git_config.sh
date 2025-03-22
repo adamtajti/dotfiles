584ee29e60f3f19558388a6b1cb01dce97057541
Adam Tajti <adam.tajti@gmail.com>
2025-03-22 10:51:59 +0100

    gentoo mess, gotta clean this up someday

--- scripts/bootstrappers/base_git_config.sh
+++ scripts/bootstrappers/base_git_config.sh
@@ -2,4 +2,5 @@
 
 # Configure Git
 # TODO: Ensure that git is installed to run this command
-git config --global hub.protocol ssh
+git config --global hub.protocol ssh # prefer ssh
+git config --global core.pager 'less -F' # don't use pager for single screen
